497edad5709072e51091674c50fa5c866e6afcdf
mar426 <78439770+mar426@users.noreply.github.com>
2021-12-15 18:22:58 +0200

    routing

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import express from 'express'
+import express from "express";
 import bodyParser from 'body-parser'
 
+import usersRout from './routes/users.js'
 const app=express()
-const PORT=3000
+const PORT=5000
 app.use(bodyParser.json())
+app.use('/users', usersRout)
+app.get('/', (req, res) => {
+    console.log('test')
+    res.send('hello world')
+})
 
 app.listen(PORT, ()=> console.log(`server running on port: http://localhost:${PORT}`))
 
