62bd253d7906deab77d394c9af88f55f3664127d
YaelSabag <yaelsabag304@gmail.com>
2021-12-15 16:15:50 +0200

    HTTP Connection

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+const app=express()
+const PORT=3000
+app.use(bodyParser.json())
+
+app.listen(PORT, ()=> console.log(`server running on port: http://localhost:${PORT}`))
+
+
+
+
+
