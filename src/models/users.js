6096bcebba54e6c371d37e9f6645be3c99a7e926
mar426 <78439770+mar426@users.noreply.github.com>
2021-12-27 16:36:48 +0200

    user schema 2

--- src/models/users.js
+++ src/models/users.js
@@ -2,5 +2,28 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-
+    ID:{
+        type: Number,
+        unique: true,
+        required: true
+    },
+    FirstName:{
+        type:String,
+        maxLength:15
+    },
+    LastName:{
+        type:String,
+        maxLength:15
+    },
+    telephone:{
+        type: Number
+    },
+    age:{
+        type: Number
+    },
+    height:{
+        type: Number
+    }
 })
+
+module.exports = mongoose.model('users', userSchema)
