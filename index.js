cbcdc5f8a88c4a49d86195f030a5362c3543b49e
mar426 <78439770+mar426@users.noreply.github.com>
2022-02-21 00:45:20 +0200

    heroku try 2

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ mongoose.connect(dbURI, connectionParams)
 const bodyParser =require('body-parser')
 app.use(bodyParser.json())
 
+app.set('view engine', 'ejs')
+
 // http status codes
 const statusOK = 200;
 const statusNotFound = 404;
@@ -54,87 +56,87 @@ const server = http.createServer((req, res) => {
 });
 
 
-// using an array to simulate a database for demonstration purposes
-var mockDatabase = [
-    {
-        fruit: "apple",
-        color: "red"
-    },
-    {
-        fruit: "banana",
-        color: "yellow"
-    }
-]
-
-// Handle GET (all) request
-app.get('/', function(req, res) {
-    // error checking
-    if (mockDatabase.length < 1) {
-        res.statusCode = statusNotFound;
-        res.send('Item not found');
-        return;
-    }
-    // send response
-    res.statusCode = statusOK;
-    res.send(mockDatabase);
-});
-
-// Handle GET (one) request
-app.get('/:id', function(req, res) {
-    // error checking
-    var id = req.params.id;
-    if (id < 0 || id >= mockDatabase.length) {
-        res.statusCode = statusNotFound;
-        res.send('Item not found');
-        return;
-    }
-    // send response
-    res.statusCode = statusOK;
-    res.send(mockDatabase[id]);
-});
-
-// Handle POST request
-app.post('/', function(req, res) {
-    // get data from request
-    var newObject = req.body; // TODO validate data
-    mockDatabase.push(newObject);
-    // send created item back with id included
-    var id = mockDatabase.length - 1;
-    res.statusCode = statusOK;
-    res.send(`Item added with id ${id}`);
-});
-
-// Handle PUT request
-app.put('/:id', function(req, res) {
-    // replace current object
-    var id = req.params.id;     // TODO validate id
-    var replacement = req.body; // TODO validate data
-    mockDatabase[id] = replacement;
-    // report back to the client
-    res.statusCode = statusOK;
-    res.send(`Item replaced at id ${id}`);
-});
-
-// Handle PATCH request
-app.patch('/:id', function(req, res) {
-    // update current object
-    var id = req.params.id;        // TODO validate id
-    var newColor = req.body.color; // TODO validate data
-    mockDatabase[id].color = newColor;
-    // report back to the client
-    res.statusCode = statusOK;
-    res.send(`Item updated at id ${id}`);
-});
-
-// Handle DELETE request
-app.delete('/:id', function(req, res) {
-    // delete specified item
-    var id = req.params.id;  // TODO validate id
-    mockDatabase.splice(id, 1);
-    // send response back
-    res.statusCode = statusOK;
-    res.send(`Item deleted at id ${id}`);
-});
+// // using an array to simulate a database for demonstration purposes
+// var mockDatabase = [
+//     {
+//         fruit: "apple",
+//         color: "red"
+//     },
+//     {
+//         fruit: "banana",
+//         color: "yellow"
+//     }
+// ]
+//
+// // Handle GET (all) request
+// app.get('/', function(req, res) {
+//     // error checking
+//     if (mockDatabase.length < 1) {
+//         res.statusCode = statusNotFound;
+//         res.send('Item not found');
+//         return;
+//     }
+//     // send response
+//     res.statusCode = statusOK;
+//     res.send(mockDatabase);
+// });
+//
+// // Handle GET (one) request
+// app.get('/:id', function(req, res) {
+//     // error checking
+//     var id = req.params.id;
+//     if (id < 0 || id >= mockDatabase.length) {
+//         res.statusCode = statusNotFound;
+//         res.send('Item not found');
+//         return;
+//     }
+//     // send response
+//     res.statusCode = statusOK;
+//     res.send(mockDatabase[id]);
+// });
+//
+// // Handle POST request
+// app.post('/', function(req, res) {
+//     // get data from request
+//     var newObject = req.body; // TODO validate data
+//     mockDatabase.push(newObject);
+//     // send created item back with id included
+//     var id = mockDatabase.length - 1;
+//     res.statusCode = statusOK;
+//     res.send(`Item added with id ${id}`);
+// });
+//
+// // Handle PUT request
+// app.put('/:id', function(req, res) {
+//     // replace current object
+//     var id = req.params.id;     // TODO validate id
+//     var replacement = req.body; // TODO validate data
+//     mockDatabase[id] = replacement;
+//     // report back to the client
+//     res.statusCode = statusOK;
+//     res.send(`Item replaced at id ${id}`);
+// });
+//
+// // Handle PATCH request
+// app.patch('/:id', function(req, res) {
+//     // update current object
+//     var id = req.params.id;        // TODO validate id
+//     var newColor = req.body.color; // TODO validate data
+//     mockDatabase[id].color = newColor;
+//     // report back to the client
+//     res.statusCode = statusOK;
+//     res.send(`Item updated at id ${id}`);
+// });
+//
+// // Handle DELETE request
+// app.delete('/:id', function(req, res) {
+//     // delete specified item
+//     var id = req.params.id;  // TODO validate id
+//     mockDatabase.splice(id, 1);
+//     // send response back
+//     res.statusCode = statusOK;
+//     res.send(`Item deleted at id ${id}`);
+// });
 
 
 //routes
@@ -146,6 +148,8 @@ app.get('/', (req, res) => {
     console.log('test')
     res.send('hello world')
 })
+
+
 
 const port =  process.env.PORT || 4000
 app.listen(port, () => {
