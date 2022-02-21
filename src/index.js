d2930831c1eb1bbf4aaeb7f959d2eae0a545bd5e
mar426 <78439770+mar426@users.noreply.github.com>
2022-02-21 22:57:13 +0200

    home page1

---
+++ src/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+//const PORT = process.env.PORT || 3000
+const http = require('http')
+const Attraction =require('./models/attraction');
+const Individual =require('./controllers/individualController');
+const port =  process.env.PORT || 3000
+
+//env
+const dotenv=require('dotenv')
+//dotenv.config()
+const isProd = process.env.APP_ENV === 'prod'
+if(!isProd)
+    dotenv.config({path:'.env'})
+
+
+
+
+//connect to mongoDB:
+const  dbURI = 'mongodb+srv://margalit:cy426316@teampark.lohoh.mongodb.net/TeamPark?retryWrites=true&w=majority'
+const mongoose = require ('mongoose')
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(dbURI, connectionParams)
+    .then(() => {
+        console.log('connected');
+
+    }).catch((err) => {
+    console.log(`error connecting ${err}`);
+})
+
+// mongoose.connect(process.env.dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+//     .then((result) => {console.log('connected')})
+//     .catch ((err)=>console.log(err))
+
+//body parser:
+const bodyParser =require('body-parser')
+app.use(bodyParser.json())
+
+//app.engine('html', require('ejs').renderFile);
+app.use(express.static("src/views/"));
+app.set('views engine', 'ejs')
+
+// http status codes
+const statusOK = 200;
+const statusNotFound = 404;
+
+const hostname = '127.0.0.1';
+//const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+app.listen(port, () => {
+    console.log(`\nserver is up and running at: http://127.0.0.1:${port}\n` )
+})
+
+//routes
+app.use('/users', require('../routes/usersAPI'))
+app.use('/attraction', require('../routes/attractionAPI'))
+app.use('/individual',require('../routes/individualAPI'))
+
+app.get('/', (req,res) => {
+    res.json({name:"rrr"})
+    //res.render('homePage');
+});
+
+
+
+
+
+
+//app.listen(PORT, hostname, ()=> console.log(`server running at: http://${hostname}:${PORT}`))
+
+
+
+
+
+
+
