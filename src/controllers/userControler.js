f08b3c384414449d1347e89199931f3b8f5d071b
YaelSabag <yaelsabag304@gmail.com>
2022-02-22 14:50:16 +0200

    update

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -64,19 +64,19 @@ const addUser= (req,res,next)=>{
     console.log("params", req.params)
     console.log("query", req.query)
     const user=new User({
-        // userID:req.query.userID,
-        // fullName:req.query.fullName,
-        // email:req.query.email,
-        // password:req.query.password,
-        // age:req.query.age,
-        // height:req.query.height
-        userID:222,
-        fullName:"gal",
-        email:"gal@gmail.com",
-        password:"123456",
-        age:22,
-        height:1.78,
-        selected_attractions:[5,4,2,3]
+        userID:req.query.userID,
+        fullName:req.query.fullName,
+        email:req.query.email,
+        password:req.query.password,
+        age:req.query.age,
+        height:req.query.height
+        // userID:222,
+        // fullName:"gal",
+        // email:"gal@gmail.com",
+        // password:"123456",
+        // age:22,
+        // height:1.78,
+        // selected_attractions:[5,4,2,3]
     })
     user.save()
         .then(response=>{
